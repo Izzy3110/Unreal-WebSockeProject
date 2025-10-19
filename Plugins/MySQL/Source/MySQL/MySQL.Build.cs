65662a1b35b76dca2c427187f067d2efed0c5161
Sascha Frank <sfrank@wyl-online.de>
2025-10-19 03:42:16 +0200

    fixed mysql build

--- Plugins/MySQL/Source/MySQL/MySQL.Build.cs
+++ Plugins/MySQL/Source/MySQL/MySQL.Build.cs
@@ -1,120 +1,102 @@
-// Copyright Athian Games. All Rights Reserved. 
+// Copyright Athian Games. All Rights Reserved.
 
 using UnrealBuildTool;
 using System.IO;
 
-
 public class MySQL : ModuleRules
 {
+    private string ThirdPartyPath => Path.GetFullPath(Path.Combine(ModuleDirectory, "../../ThirdParty/"));
 
-    private string ThirdPartyPath
+    public string ProjectBinariesPath =>
+        Path.Combine(Target.ProjectFile!.Directory.FullName, "Binaries", Target.Platform.ToString());
+
+    private void CopyToBinaries(string filePath, string destination, ReadOnlyTargetRules target)
     {
-        get { return Path.GetFullPath(Path.Combine(ModuleDirectory, "../../ThirdParty/")); }
-    }
+        _ = target; // explicitly mark as intentionally unused
 
-    public string ProjectBinariesPath
-    {
-
-        get { return Path.Combine(Target.ProjectFile!.Directory.FullName, "Binaries", Target.Platform.ToString()); }
-    }
-
-    private void CopyToBinaries(string FilePath, string destination, ReadOnlyTargetRules Target)
-    {
-        
-        string Filename = Path.GetFileName(FilePath);
-        string DestPath = Path.Combine(destination, Filename);
+        var fileName = Path.GetFileName(filePath);
+        var destPath = Path.Combine(destination, fileName);
 
         if (!Directory.Exists(destination))
-        {
             Directory.CreateDirectory(destination);
-        }
 
-        if (!File.Exists(DestPath))
-        {
-            File.Copy(FilePath, DestPath, true);
-        }
-        
-        RuntimeDependencies.Add(DestPath);
-        PublicDelayLoadDLLs.Add(Path.Combine(DestPath));
+        if (!File.Exists(destPath))
+            File.Copy(filePath, destPath, true);
 
+        RuntimeDependencies.Add(destPath);
+        PublicDelayLoadDLLs.Add(destPath);
     }
-    public MySQL(ReadOnlyTargetRules Target) : base(Target)
+
+    public MySQL(ReadOnlyTargetRules target) : base(target)
     {
+        // Core include paths and dependencies
+        PrivateIncludePaths.AddRange(["MySql/Private"]);
 
-        PrivateIncludePaths.AddRange(new string[]
-        {
-            "MySQL/Private"
-
-        });
-
-    
-        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-
-        UndefinedIdentifierWarningLevel = WarningLevel.Off;
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+        CppCompileWarningSettings.UndefinedIdentifierWarningLevel = WarningLevel.Off;
         bEnableExceptions = true;
-        
-        PublicDependencyModuleNames.AddRange(new[] { "Core", "CoreUObject", "Engine", "RHI",
-            "ImageWrapper", "RenderCore", "ImageWriteQueue", "InputCore" , "Projects" });
-        PrivateDependencyModuleNames.AddRange(new[] { "XmlParser", "Core", "ImageWrapper", "Engine" });
 
-        if (Target.Platform == UnrealTargetPlatform.Win64 || Target.Platform == UnrealTargetPlatform.LinuxArm64 ||
-            Target.Platform == UnrealTargetPlatform.Linux)
+        PublicDependencyModuleNames.AddRange([
+            "Core", "CoreUObject", "Engine", "RHI",
+            "ImageWrapper", "RenderCore", "ImageWriteQueue",
+            "InputCore", "Projects"
+        ]);
+
+        PrivateDependencyModuleNames.AddRange([
+            "XmlParser", "Core", "ImageWrapper", "Engine"
+        ]);
+
+        // Supported platforms only
+        if (target.Platform != UnrealTargetPlatform.Win64 &&
+            target.Platform != UnrealTargetPlatform.LinuxArm64 &&
+            target.Platform != UnrealTargetPlatform.Linux)
         {
-            string platformString = "";
-            string libExtension = "";
-            string binExtension = "";
-
-            if (Target.Platform == UnrealTargetPlatform.Win64)
-            {
-                platformString = "Win64";
-                libExtension = ".lib";
-                binExtension = ".dll";
-                PublicDefinitions.Add("NTDDI_WIN7SP1");
-            }
-            else if (Target.Platform == UnrealTargetPlatform.LinuxArm64)
-            {
-                platformString = "Linux-arm64";
-                libExtension = ".so";
-                binExtension = ".so";
-            }
-            else if (Target.Platform == UnrealTargetPlatform.Linux)
-            {
-                platformString = "Linux-x64";
-                libExtension = ".so";
-                binExtension = ".so";
-            }
-          
-
-            string mySQLPath = Path.Combine(ThirdPartyPath, "MariaDB");
-            string mySQLlibraryPath = Path.Combine(mySQLPath, "lib", platformString);
-            string BinariesDir = ProjectBinariesPath;
-            
-            string pluginpath = Path.Combine(mySQLlibraryPath, "plugin");
-
-            PublicIncludePaths.Add(Path.Combine(mySQLPath, "include"));
-            PublicSystemLibraryPaths.Add(mySQLlibraryPath);
-
-            foreach (string file in Directory.GetFiles(mySQLlibraryPath, "*" + libExtension))
-            {
-               PublicAdditionalLibraries.Add(file);
-            }
-            
-            foreach (string file in Directory.GetFiles(mySQLlibraryPath, "*" + binExtension))
-            {
-                CopyToBinaries(file, BinariesDir, Target);
-            }
-            
-            foreach (string file in Directory.GetFiles(pluginpath, "*" + libExtension))
-            {
-                PublicAdditionalLibraries.Add(file);
-            }
-
-            foreach (string file in Directory.GetFiles(pluginpath, "*" + binExtension))
-            {
-                CopyToBinaries(file, BinariesDir, Target);
-            }
-
+            return;
         }
 
+        var platformString = "";
+        var libExtension = "";
+        var binExtension = "";
+
+        if (target.Platform == UnrealTargetPlatform.Win64)
+        {
+            platformString = "Win64";
+            libExtension = ".lib";
+            binExtension = ".dll";
+            PublicDefinitions.Add("NTDDI_WIN7SP1");
+        }
+        else if (target.Platform == UnrealTargetPlatform.LinuxArm64)
+        {
+            platformString = "Linux-arm64";
+            libExtension = ".so";
+            binExtension = ".so";
+        }
+        else if (target.Platform == UnrealTargetPlatform.Linux)
+        {
+            platformString = "Linux-x64";
+            libExtension = ".so";
+            binExtension = ".so";
+        }
+
+        var mySqlPath = Path.Combine(ThirdPartyPath, "MariaDB");
+        var mySqlLibraryPath = Path.Combine(mySqlPath, "lib", platformString);
+        var binariesDir = ProjectBinariesPath;
+        var pluginPath = Path.Combine(mySqlLibraryPath, "plugin");
+
+        PublicIncludePaths.Add(Path.Combine(mySqlPath, "include"));
+        PublicSystemLibraryPaths.Add(mySqlLibraryPath);
+
+        // Add static and dynamic libraries
+        foreach (var file in Directory.GetFiles(mySqlLibraryPath, "*" + libExtension))
+            PublicAdditionalLibraries.Add(file);
+
+        foreach (var file in Directory.GetFiles(mySqlLibraryPath, "*" + binExtension))
+            CopyToBinaries(file, binariesDir, target);
+
+        foreach (var file in Directory.GetFiles(pluginPath, "*" + libExtension))
+            PublicAdditionalLibraries.Add(file);
+
+        foreach (var file in Directory.GetFiles(pluginPath, "*" + binExtension))
+            CopyToBinaries(file, binariesDir, target);
     }
 }
