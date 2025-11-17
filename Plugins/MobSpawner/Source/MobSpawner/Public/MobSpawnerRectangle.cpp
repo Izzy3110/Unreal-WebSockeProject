de91753587d7468c796f80b93f8c5b7d18af51e5
Sascha Frank <sfrank@wyl-online.de>
2025-11-17 15:03:33 +0100

    MobSpawner: Debug Functions added

--- Plugins/MobSpawner/Source/MobSpawner/Public/MobSpawnerRectangle.cpp
+++ Plugins/MobSpawner/Source/MobSpawner/Public/MobSpawnerRectangle.cpp
@@ -1,10 +1,11 @@
-﻿// MobSpawnerRectangle.cpp
-#include "MobSpawnerRectangle.h"
+﻿#include "MobSpawnerRectangle.h"
 #include "Components/SplineComponent.h"
+#include "DrawDebugHelpers.h"
+#include "Math/UnrealMathUtility.h"
 
 AMobSpawnerRectangle::AMobSpawnerRectangle()
 {
-	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bCanEverTick = true; // needed for runtime debug updates
 
 	Spline = CreateDefaultSubobject<USplineComponent>(TEXT("Spline"));
 	RootComponent = Spline;
@@ -16,18 +17,100 @@ void AMobSpawnerRectangle::OnConstruction(const FTransform& Transform)
 
 	Spline->ClearSplinePoints(false);
 
-	const FVector Half = FVector(Size.X / 2, Size.Y / 2, 0.f);
+	const FVector Half = FVector(Size.X / 2.f, Size.Y / 2.f, 0.f);
 
-	Spline->AddSplinePoint(FVector(-Half.X, -Half.Y, 0), ESplineCoordinateSpace::Local);
-	Spline->AddSplinePoint(FVector(Half.X, -Half.Y, 0), ESplineCoordinateSpace::Local);
-	Spline->AddSplinePoint(FVector(Half.X, Half.Y, 0), ESplineCoordinateSpace::Local);
-	Spline->AddSplinePoint(FVector(-Half.X, Half.Y, 0), ESplineCoordinateSpace::Local);
-	
-	if (ClosedLoopRequired)
+	Spline->AddSplinePoint(FVector(-Half.X, -Half.Y, 0.f), ESplineCoordinateSpace::Local);
+	Spline->AddSplinePoint(FVector(Half.X, -Half.Y, 0.f), ESplineCoordinateSpace::Local);
+	Spline->AddSplinePoint(FVector(Half.X, Half.Y, 0.f), ESplineCoordinateSpace::Local);
+	Spline->AddSplinePoint(FVector(-Half.X, Half.Y, 0.f), ESplineCoordinateSpace::Local);
+
+	if (ClosedLoopRequired && !Spline->IsClosedLoop())
 	{
-		if (Spline->IsClosedLoop() == false)
-			Spline->SetClosedLoop(true);
+		Spline->SetClosedLoop(true);
 	}
-	
+
 	Spline->UpdateSpline();
+
+	// Draw debug in the editor or runtime if enabled
+	if (bDrawDebug)
+	{
+		DrawDebugRectangle();
+	}
+}
+
+void AMobSpawnerRectangle::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Draw debug immediately at "BeginPlay" if enabled
+	if (bDrawDebug)
+	{
+		DrawDebugRectangle();
+	}
+}
+
+void AMobSpawnerRectangle::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	// Continuous runtime debug
+	if (bDrawDebug && bDrawDebugEveryTick)
+	{
+		DrawDebugRectangle();
+	}
+}
+
+bool AMobSpawnerRectangle::IsPointInsideSpline(const FVector& Point) const
+{
+	const FVector LocalPoint = GetActorTransform().InverseTransformPosition(Point);
+
+	const float HalfX = Size.X / 2.f;
+	const float HalfY = Size.Y / 2.f;
+
+	const bool bInside = (LocalPoint.X >= -HalfX && LocalPoint.X <= HalfX) &&
+						 (LocalPoint.Y >= -HalfY && LocalPoint.Y <= HalfY);
+
+	if (bDrawDebug)
+	{
+		DrawDebugSphere(GetWorld(), Point, DebugPointSize, 12, bInside ? FColor::Green : FColor::Red, false, 2.f);
+	}
+
+	return bInside;
+}
+
+FVector AMobSpawnerRectangle::GetRandomPointInsideSpline() const
+{
+	const float HalfX = Size.X / 2.f;
+	const float HalfY = Size.Y / 2.f;
+
+	const float RandomX = FMath::FRandRange(-HalfX, HalfX);
+	const float RandomY = FMath::FRandRange(-HalfY, HalfY);
+
+	const FVector RandomPoint = GetActorTransform().TransformPosition(FVector(RandomX, RandomY, 0.f));
+
+	if (bDrawDebug)
+	{
+		DrawDebugSphere(GetWorld(), RandomPoint, DebugPointSize, 12, FColor::Red, false, 2.f);
+	}
+
+	return RandomPoint;
+}
+
+void AMobSpawnerRectangle::DrawDebugRectangle() const
+{
+	if (!bDrawDebug) return;
+
+	const FVector Half = FVector(Size.X / 2.f, Size.Y / 2.f, 0.f);
+	const FVector WorldPoints[4] = {
+		GetActorTransform().TransformPosition(FVector(-Half.X, -Half.Y, 0.f)),
+		GetActorTransform().TransformPosition(FVector(Half.X, -Half.Y, 0.f)),
+		GetActorTransform().TransformPosition(FVector(Half.X, Half.Y, 0.f)),
+		GetActorTransform().TransformPosition(FVector(-Half.X, Half.Y, 0.f))
+	};
+
+	// Draw rectangle edges
+	for (int32 i = 0; i < 4; i++)
+	{
+		DrawDebugLine(GetWorld(), WorldPoints[i], WorldPoints[(i + 1) % 4], DebugColor, false, 0.f, 0, 2.f);
+	}
 }
--- Plugins/MobSpawner/Source/MobSpawner/Public/MobSpawnerRectangle.h
+++ Plugins/MobSpawner/Source/MobSpawner/Public/MobSpawnerRectangle.h
@@ -15,22 +15,52 @@ class MOBSPAWNER_API AMobSpawnerRectangle : public AActor
 public:
 	AMobSpawnerRectangle();
 
-
-	UPROPERTY(BlueprintReadWrite, Category="Spawner")
-	float SizeX_Default = 1000.f;
-
-	UPROPERTY(BlueprintReadWrite, Category="Spawner")
-	float SizeY_Default = 500.f;
-	
+	/** Rectangle size */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Spawner")
-	FVector2D Size = FVector2D(SizeX_Default, SizeY_Default);
+	FVector2D Size = FVector2D(1000.f, 500.f);
 
+	/** Spline component representing rectangle */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	USplineComponent* Spline;
 
+	/** Whether the rectangle spline should be closed */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Spawner")
 	bool ClosedLoopRequired = true;
 
+	/** Checks if a point is inside the rectangle */
+	UFUNCTION(BlueprintCallable, Category="Spawner")
+	bool IsPointInsideSpline(const FVector& Point) const;
+
+	/** Returns a random point inside the rectangle */
+	UFUNCTION(BlueprintCallable, Category="Spawner")
+	FVector GetRandomPointInsideSpline() const;
+
+	// ---------------------------
+	// Debug options
+	// ---------------------------
+
+	/** Whether to draw debug visualization */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Spawner|Debug")
+	bool bDrawDebug = true;
+
+	/** Debug color for lines and points */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Spawner|Debug")
+	FColor DebugColor = FColor::Green;
+
+	/** Debug sphere radius for points */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Spawner|Debug")
+	float DebugPointSize = 10.f;
+
+	/** Draw debug rectangle and points in the editor or runtime */
+	UFUNCTION(CallInEditor, BlueprintCallable, Category="Spawner|Debug")
+	void DrawDebugRectangle() const;
+
+	/** Whether to update debug every tick (runtime only) */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Spawner|Debug")
+	bool bDrawDebugEveryTick = false;
+
 protected:
 	virtual void OnConstruction(const FTransform& Transform) override;
+	virtual void Tick(float DeltaTime) override;
+	virtual void BeginPlay() override;
 };
