b8f58a415ed5a7e2380d570d2eb9288b7031b63d
Sascha Frank <sfrank@wyl-online.de>
2025-11-17 14:42:14 +0100

    success: Stage #1 - Spline creation with Initial Rectangle and Closed Loop enabled

---
+++ Plugins/MobSpawner/Source/MobSpawner/Public/MobSpawnerRectangle.cpp
@@ -0,0 +1,33 @@
+﻿// MobSpawnerRectangle.cpp
+#include "MobSpawnerRectangle.h"
+#include "Components/SplineComponent.h"
+
+AMobSpawnerRectangle::AMobSpawnerRectangle()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Spline = CreateDefaultSubobject<USplineComponent>(TEXT("Spline"));
+	RootComponent = Spline;
+}
+
+void AMobSpawnerRectangle::OnConstruction(const FTransform& Transform)
+{
+	if (!Spline) return;
+
+	Spline->ClearSplinePoints(false);
+
+	const FVector Half = FVector(Size.X / 2, Size.Y / 2, 0.f);
+
+	Spline->AddSplinePoint(FVector(-Half.X, -Half.Y, 0), ESplineCoordinateSpace::Local);
+	Spline->AddSplinePoint(FVector(Half.X, -Half.Y, 0), ESplineCoordinateSpace::Local);
+	Spline->AddSplinePoint(FVector(Half.X, Half.Y, 0), ESplineCoordinateSpace::Local);
+	Spline->AddSplinePoint(FVector(-Half.X, Half.Y, 0), ESplineCoordinateSpace::Local);
+	
+	if (ClosedLoopRequired)
+	{
+		if (Spline->IsClosedLoop() == false)
+			Spline->SetClosedLoop(true);
+	}
+	
+	Spline->UpdateSpline();
+}
